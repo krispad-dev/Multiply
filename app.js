6a0d5bbf969d42ba9e8bfa6fce0eee78a27ae911
krispad-dev <kristofer.padoan@icloud.com>
2021-03-10 22:19:31 +0100

    added some arrays

--- app.js
+++ app.js
@@ -2,3 +2,14 @@ const multiplyer = (value) => { return value * 2 }
 
 console.log(multiplyer(10));
 
+
+// Added data 
+
+
+const planets = ['Mars', 'Neptune', 'saturn', 'Jupiter', 'Venus', 'Uranus'];
+const stars = ['Sun', 'Betelgeuse', 'VY Canis Majoris', 'Alpha Centauri'];
+
+const starsAndPlanets = [...planets, ...stars];
+console.log(starsAndPlanets);
+
+
